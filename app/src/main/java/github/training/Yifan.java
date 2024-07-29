8bbad2d687a691da31ce55b6f2bb1d3d89e390d3
Yifinity <92883414+Yifinity@users.noreply.github.com>
2024-07-28 22:28:29 -0400

    First Commit

---
+++ app/src/main/java/github/training/Yifan.java
@@ -0,0 +1,13 @@
+package github.training;
+
+public class Yifan {
+    // Adjust the value based on you
+    private final String name = "Yifan";
+    private final int grade = 12; 
+
+    public void sayGreeting(){
+        // Make sure you change 
+        System.out.print("Hello! My Name is " + name);
+        System.out.println(" and this year I will be in Grade " + grade);
+    }
+}
